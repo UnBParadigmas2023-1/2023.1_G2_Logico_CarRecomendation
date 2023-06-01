4e326de6dd3eded42e1e7e7e64e189bff6142188
vital14 <limavictor49@gmail.com>
2023-06-01 12:48:29 -0300

    ajustando recomendação por combustível para o padrão

--- src/recomendacoes/tipo_combustivel.pl
+++ src/recomendacoes/tipo_combustivel.pl
@@ -1,9 +1,26 @@
 use_module(library(readutil)).
 
-:- [src/data/dados_prolog].
-:- (dynamic fuel/1, car/8).
+:- consult('./../data/dados_prolog.pl'). 
 
 
 % Predicado para recomendar veículos com base no tipo de combustível
-recommend_vehicle_by_fuel(Fuel ) :-
-    findall(Car,car(_,_,_,_,Fuel,_,_,_),Cars),.
+recomendaCombustivel(Combustivel) :-
+    findall(Name/[Preco, Combustivel, Transmissao], car(Name, Ano, Preco, _, Combustivel, _, Transmissao, _), Resultado),
+    maplist(converter_para_par, Resultado, Pares),
+    keysort(Pares, Sorted),
+    imprimirResultado(Sorted).
+
+
+
+
+
+
+% Regra de imprimir o resultado
+imprimirResultado([]).
+imprimirResultado([Item | Resto]) :-
+    write(Item),
+    nl,
+    imprimirResultado(Resto).
+
+% Regra de conversão para o maplist
+converter_para_par(Chave/Itens, Chave-Itens).
