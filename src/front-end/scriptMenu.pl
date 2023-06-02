4ae5481174bce6a20e41eb514a217e504cb70643
Gustavo Martins <72039007+gustavomartins-github@users.noreply.github.com>
2023-06-01 23:08:50 -0300

    Esqueleto da interface gráfica
    Finalização da estrutura geral da interface gráfica do projeto por meio do winprolog: criação da janela, componentes e conteúdo, além de suas respectivas posições na matrix de pixels.

--- src/front-end/scriptMenu.pl
+++ src/front-end/scriptMenu.pl
@@ -1,22 +1,26 @@
-?- wcreate(menu,dialog,`Car Recommendation`,200,250,400,225,16'80c80000).
-?- wcreate((menu,1),button,`&Search`,10,150,80,32,16'50010000).
-?- wcreate((menu,2),button,`&Cancel`,100,150,80,32,16'50010000).
 
-% quando o botão OK for clicado, retorne o texto na janela de edição
-menu_handler( (menu,1), msg_button, _, Text ) :-
-wtext( (menu,100), Text ).
+% CRIANDO A JANELA DE DIALOGO
+?- wcreate(menu,dialog,`Car Recommendation`,800,1000,1600,900,16'80c80000).
 
-% quando o botão Cancelar for clicado, retorne o átomo "cancel"
-menu_handler( (menu,2), msg_button, _, cancel ).
+% CRIANDO OS COMPONENTES DA JANELA DE DIALOGO
 
-% quando o ícone fechar é clicado duas vezes, mostra uma caixa de mensagem
-menu_handler( menu, msg_close, _, _ ) :-
-msgbox( 'Dear User!', 'Thank you and come back often!', 48, _ ).
+?- wcreate((menu,1),button,`&Cancel`,930,730,300,64,16'50080000).
+?- wcreate((menu,2),button,`&Search`,400,730,300,64,16'50010000). 
 
-% ignorando todas as outras mensagens
-menu_handler( _, _, _, _ ).
+?- wcreate((menu,3),combobox,``,130,187,240,192,16'50a10002).
+?- wcreate((menu,4),combobox,``,400,187,240,192,16'50a10002).
+?- wcreate((menu,5),combobox,``,670,187,240,192,16'50a10002).
+?- wcreate((menu,6),combobox,``,940,187,240,192,16'50a10002).
+?- wcreate((menu,7),combobox,``,1210,187,240,192,16'50a10002).
 
-?- window_handler(menu,menu_handler).
+?- wcreate((menu,8),listbox,``,300,250,1050,400,16'50a10002).
 
-% exibindo a janela de dialogo
+?- wcreate((menu,9),static,`FIELD`,230,100,240,64,16'50080000).
+?- wcreate((menu,10),static,`TRANSMISSION`,480,100,240,64,16'50080000).
+?- wcreate((menu,11),static,`MODEL`,765,100,240,64,16'50080000).
+?- wcreate((menu,12),static,`YEAR`,1040,100,240,64,16'50080000).
+?- wcreate((menu,13),static,`PRICE`,1310,100,240,64,16'50080000).
+
+% EXIBINDO A JANELA DE DIALOGO
 ?- call_dialog(menu,X).
+
