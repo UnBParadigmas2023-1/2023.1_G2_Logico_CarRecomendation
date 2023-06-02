25a7503d3956b451a7d06a176ee7beb48efec94d
RafaelBP02 <rafaelbertopereira@gmail.com>
2023-06-02 18:19:47 -0300

    Correcao do WARNING
    Co-authored-by: matheuscvp <matheuscvp12@gmail.com>
    Co-authored-by: vital14 <limavictor49@gmail.com>
    Co-authored-by: brenno-silva <brenno.silva037@gmail.com>
    Co-authored-by: Gustavo Martins <200019228@aluno.unb.br>

--- src/recomendacoes/tipo_combustivel.pl
+++ src/recomendacoes/tipo_combustivel.pl
@@ -1,26 +1,16 @@
-use_module(library(readutil)).
-
 :- consult('./../data/dados_prolog.pl'). 
-
+:- [utils].
 
 % Predicado para recomendar veículos com base no tipo de combustível
-recomendaCombustivel(Combustivel) :-
-    findall(Name/[Preco, Combustivel, Transmissao], car(Name, Ano, Preco, _, Combustivel, _, Transmissao, _), Resultado),
-    maplist(converter_para_par, Resultado, Pares),
-    keysort(Pares, Sorted),
-    imprimirResultado(Sorted).
+recomendaCombustivel(Combustivel, Resposta) :-
+    findall(Name-[Ano, Preco, Combustivel, Transmissao], car(Name, Ano, Preco, _, Combustivel, _, Transmissao, _), Resultado),
+
+    keysort(Resultado, Sorted),
+
+    Resposta = Sorted.
 
 
+imprimirResultadoTransmissao(Combustivel) :-
+    recomendaCombustivel(Combustivel, Resposta),
+    imprimirResultado(Resposta).
 
-
-
-
-% Regra de imprimir o resultado
-imprimirResultado([]).
-imprimirResultado([Item | Resto]) :-
-    write(Item),
-    nl,
-    imprimirResultado(Resto).
-
-% Regra de conversão para o maplist
-converter_para_par(Chave/Itens, Chave-Itens).
--- src/recomendacoes/tipo_transmissao.pl
+++ src/recomendacoes/tipo_transmissao.pl
@@ -1,17 +1,12 @@
 :- consult('./../data/dados_prolog.pl'). %fatos do recomendador
+:- [utils].
 
 recommend_cars_with_transmission(Transmissao, Resposta) :-
-    findall(Name-[Preco,Combustivel,Transmissao], car(Name, _, Preco, _, Combustivel, _, Transmissao, _), Resultado),
+    findall(Name-[Ano, Preco,Combustivel,Transmissao], car(Name, Ano, Preco, _, Combustivel, _, Transmissao, _), Resultado),
 
     keysort(Resultado, Sorted),
 
     Resposta = Sorted.
-
-imprimirResultado([]).
-imprimirResultado([Item | Resto]) :-
-    write(Item),
-    nl,
-    imprimirResultado(Resto).
 
 imprimirResultadoTransmissao(Transmissao) :-
     recommend_cars_with_transmission(Transmissao, Resposta),
