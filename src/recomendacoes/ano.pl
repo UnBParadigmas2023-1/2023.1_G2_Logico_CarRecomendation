a47da4a6eb479ec548980b93e2d4745900d005cc
RafaelBP02 <rafaelbertopereira@gmail.com>
2023-06-01 22:46:08 -0300

    Adapta os modulos de marca e ano para permitir interseccao entre eles. Cria o modulo utils

--- src/recomendacoes/ano.pl
+++ src/recomendacoes/ano.pl
@@ -1,41 +1,38 @@
 :- consult('./../data/dados_prolog.pl'). %fatos do recomendador
+:- [utils].
 
 
 % car(Maruti 800 AC, 2007, 60000, 70000, Petrol, Individual, Manual, First Owner).
-recomendaAno(Ano) :- 
-    findall(Name/[Preco,Combustivel,Transmissao], car(Name,Ano,Preco,_,Combustivel,_,Transmissao,_), Resultado),
-    
-    % converter a lista de resultados em uma lista de pares chave-valor
-    maplist(converter_para_par, Resultado, Pares),
+recomendaAno(Ano, Resposta) :- 
+    findall(Name-[Ano,Preco,Combustivel,Transmissao], car(Name,Ano,Preco,_,Combustivel,_,Transmissao,_), Resultado),
     
     % ordenar a lista
-    keysort(Pares, Sorted),
+    keysort(Resultado, Sorted),
     
     % imprimir lista ordenada
-    imprimirResultado(Sorted).
+    Resposta = Sorted.
 
 
 % Regra para buscar um certo intervalo em anos
-recomendaAnoIntervalo(AnoInicial, AnoFinal) :- 
-    findall(Name/[Ano,Preco,Combustivel,Transmissao], (car(Name,Ano,Preco,_,Combustivel,_,Transmissao,_), Ano >= AnoInicial, Ano =< AnoFinal), Resultado),
+recomendaAnoIntervalo(AnoInicial, AnoFinal, Resposta) :- 
+    findall(Name-[Ano,Preco,Combustivel,Transmissao], (
+        car(Name,Ano,Preco,_,Combustivel,_,Transmissao,_),
+         Ano >= AnoInicial, Ano =< AnoFinal
+    ), Resultado),
     %(Item, (sua_regra(Item), Item >= LimiteInferior, Item =< LimiteSuperior)
     
-    
-    % converter a lista de resultados em uma lista de pares chave-valor
-    maplist(converter_para_par, Resultado, Pares),
-    
     % ordenar a lista
-    keysort(Pares, Sorted),
+    keysort(Resultado, Sorted),
     
-    % imprimir lista ordenada
-    imprimirResultado(Sorted).
+    Resposta = Sorted.
+  
 
-% Regra de imprimir o resultado
-imprimirResultado([]).
-imprimirResultado([Item | Resto]) :-
-    write(Item),
-    nl,
-    imprimirResultado(Resto).
+% imprimir lista ordenada
+imprimirResultadoAno(Ano) :- 
+    recomendaAno(Ano, Resposta),
+    imprimirResultado(Resposta).
 
-% Regra de conversão para o maplist
-converter_para_par(Chave/Itens, Chave-Itens).
+% imprimir lista ordenada
+imprimirResultadoAno2(AnoInicial, AnoFinal) :- 
+    recomendaAnoIntervalo(AnoInicial, AnoFinal, Resposta),
+    imprimirResultado(Resposta).
--- src/recomendacoes/marca.pl
+++ src/recomendacoes/marca.pl
@@ -1,9 +1,10 @@
 :- consult('./../data/dados_prolog.pl'). %fatos do recomendador
 :- [ano].
+:- [utils].
 
 
 % car(Maruti 800 AC, 2007, 60000, 70000, Petrol, Individual, Manual, First Owner).
-recomendaMarca(Marca) :- 
+recomendaMarca(Marca, Resposta) :- 
     findall(Nome-[Ano,Preco,Combustivel,Transmissao], (
         car(Nome, Ano, Preco, _, Combustivel, _, Transmissao, _),
         sub_atom(Nome, _, _, _, Marca) %Verificar se Marca é uma substring de Nome
@@ -12,12 +13,32 @@ recomendaMarca(Marca) :-
     %ordena o resultado
     keysort(Resultado, Sorted),
 
+    Resposta = Sorted.
+
     %imprime o resultado
-    imprimirResultado(Sorted) .
+
+imprimirResultadoMarca(Marca) :- 
+    recomendaMarca(Marca, Resposta),
+    imprimirResultado(Resposta).
 
 
-imprimirResultado([]).
-imprimirResultado([Item | Resto]) :-
-    write(Item),
-    nl,
-    imprimirResultado(Resto).
+%>>>>>>>>>>>>>>>> REGRAS P/ TESTE D INTERSECÇÃO...
+imprimirIntersecaoMerge(Marca,Ano):-
+    recomendaIntersecao(Marca, Ano, Resposta),
+    imprimirResultado(Resposta).
+
+recomendaIntersecao(Marca, Ano, Resposta) :-
+    recomendaMarca(Marca, RespostaMarca),
+    recomendaAno(Ano, RespostaAno),
+    intersection(RespostaMarca, RespostaAno, Resposta).
+
+%<<<<<<<<<<<<<<<<<<<<<<<<<<< FIM
+
+
+
+
+% nl,
+    % nl,
+    % nl,
+    % nl,
+    % write('Intersecção dos resultados...'),
